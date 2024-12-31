9edd64e300b2f0c7371545dafff9778014c41995
pyke9 <89367542+pyke9@users.noreply.github.com>
2024-12-31 11:43:03 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||Chúc|Mừng|Năm|Mới|Wish you|Always||Happy|Beautiful|Smile|Lucky|Miss me!|THANKS YOU');
+      S.UI.simulate('|#countdown 3||Chúc|Mừng|Năm|Mới|Wish you|Always||Happy|Beautiful|Smile|Lucky|Miss me!|THANK YOU');
     }
 
     S.Drawing.loop(function () {
